4d36a6ff29c4815672c6feb87e04396de98cff99
nikko <35370166+nikkoxd@users.noreply.github.com>
2023-10-28 00:48:40 +0400

    Added code to send buttons

--- src/commands/addButton.ts
+++ src/commands/addButton.ts
@@ -1,8 +1,8 @@
 import { Command } from "@sapphire/framework";
-import { PermissionFlagsBits } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } from "discord.js";
 import { readFile } from "fs";
 
-export class AddComponentCommand extends Command {
+export class AddButtonCommand extends Command {
   public constructor(ctx: Command.Context, options: Command.Options) {
     super(ctx, { ...options });
   }
@@ -50,15 +50,57 @@ export class AddComponentCommand extends Command {
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
     const message = interaction.channel?.messages.fetch(interaction.options.getString('id', true));
     const label = interaction.options.getString('label', true);
-    const style = interaction.options.getString('style', true);
+    const styleValue = interaction.options.getString('style', true);
     const action = interaction.options.getString('action', true);
+    let style;
 
-    switch(label) {
+    const button = new ButtonBuilder()
+    const row = new ActionRowBuilder<ButtonBuilder>()
+
+    switch(styleValue) {
+      case 'primary':
+        style = ButtonStyle.Primary;
+        break;
+      case 'secondary':
+        style = ButtonStyle.Secondary;
+        break;
+      case 'success':
+        style = ButtonStyle.Success;
+        break;
+      case 'danger':
+        style = ButtonStyle.Danger;
+        break;
+      case 'link':
+        style = ButtonStyle.Link;
+        break;
+    }
+
+    switch(styleValue) {
       case 'primary' || 'secondary' || 'success' || 'danger':
         this.container.logger.info('Message sending button being added');
+
+        button
+          .setCustomId("sendmsg" + "-" + action)
+          .setLabel(label)
+          .setStyle(style as ButtonStyle);
+
+        row.addComponents(button);
+
+        (await message)?.edit({ components: [row] })
+
         break;
       case 'link':
         this.container.logger.info('URL sending button being added');
+
+        button
+          .setCustomId("sendmsg" + "-" + action)
+          .setLabel(label)
+          .setStyle(style as ButtonStyle);
+
+        row.addComponents(button);
+
+        (await message)?.edit({ components: [row] })
+
         break;
     }
   }
