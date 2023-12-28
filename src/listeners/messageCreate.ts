163f02c9e0319ea3134483628919e0b9c1417696
nikko <35370166+nikkoxd@users.noreply.github.com>
2023-12-28 17:43:20 +0400

    feat: reply to failed embed tenor.com links

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,5 +1,6 @@
 import { Listener } from "@sapphire/framework";
-import { Message, PermissionFlagsBits } from "discord.js";
+import { Message, PermissionFlagsBits, TextChannel } from "discord.js";
+import { t } from "i18next";
 
 export class messageCreateListener extends Listener {
   public constructor(
@@ -14,9 +15,34 @@ export class messageCreateListener extends Listener {
   }
 
   public override async run(message: Message) {
-    if (message.content.includes("@everyone" || "@here")) {
-      if (!message.member?.permissions.has(PermissionFlagsBits.MentionEveryone))
-        message.react("🤡");
+    // REACT TO @everyone AND @here
+    // =============
+    if (
+      message.content.includes("@everyone" || "@here") &&
+      !message.member?.permissions.has(PermissionFlagsBits.MentionEveryone)
+    ) {
+      message.react("🤡");
     }
+    // REPLY TO FAILED EMBED LINKS
+    // =============
+    if (
+      message.content.startsWith("https://tenor.com/") &&
+      !message.member
+        ?.permissionsIn(message.channel as TextChannel)
+        .has("EmbedLinks")
+    ) {
+      message.reply(t("listeners.messageCreate.cantSendGifs"));
+    }
+    // REPLY TO FAILED ATTACHMENTS
+    // =============
+    // if (
+    //   message.attachments &&
+    //   !message.content.startsWith("https://tenor.com") &&
+    //   !message.member
+    //     ?.permissionsIn(message.channel as TextChannel)
+    //     .has("AttachFiles")
+    // ) {
+    //   message.reply(t("listeners.messageCreate.cantAttachFiles"));
+    // }
   }
 }
