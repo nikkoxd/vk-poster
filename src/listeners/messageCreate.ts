ee2e1ed41ae1d7c816864045fbf374b549019f3f
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-01-27 23:31:24 +0400

    Revert "chore: remove unused code"
    This reverts commit 05c928e35da47de0e51aae27ac1c35220910f9f1.

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,6 +1,5 @@
 import { Listener } from "@sapphire/framework";
 import { Message, PermissionFlagsBits } from "discord.js";
-import Member from "../schemas/Member";
 
 export class messageCreateListener extends Listener {
   public constructor(
@@ -15,21 +14,6 @@ export class messageCreateListener extends Listener {
   }
 
   public override async run(message: Message) {
-    const memberId = message.author.id;
-
-    if (!message.author.bot) {
-      if ((await Member.find({ id: memberId })).length != 0) {
-        message.reply("👋");
-      } else {
-        const member = new Member({
-          id: memberId,
-          coins: 0,
-        });
-        await member.save();
-        message.reply(memberId + " добавлен в БД");
-      }
-    }
-
     if (message.content.includes("@everyone" || "@here")) {
       if (!message.member?.permissions.has(PermissionFlagsBits.MentionEveryone))
         message.react("🤡");
