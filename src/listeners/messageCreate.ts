a57cf87ec4f7e0eebc10ea9b0313b96a69227814
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-01-28 00:34:18 +0400

    feat: add coins for each message sent

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,5 +1,7 @@
 import { Listener } from "@sapphire/framework";
 import { Message, PermissionFlagsBits } from "discord.js";
+import Member from "../schemas/Member";
+import { logError } from "..";
 
 export class messageCreateListener extends Listener {
   public constructor(
@@ -13,7 +15,35 @@ export class messageCreateListener extends Listener {
     });
   }
 
+  async getRandomCoins() {
+    const min = Number(process.env.COINS_MIN);
+    const max = Number(process.env.COINS_MAX);
+    if (min && max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    } else {
+      logError(
+        "MIN and MAX values for coins not found, giving predefined amount",
+      );
+      return Math.floor(Math.random() * (85 - 50) + 50);
+    }
+  }
+
   public override async run(message: Message) {
+    const memberId = message.author.id;
+    const member = await Member.findOne({ memberId: memberId });
+
+    if (!message.author.bot) {
+      const addedCoins = await this.getRandomCoins();
+      if (member) {
+        await Member.updateOne(
+          { memberId: memberId },
+          { coins: member.coins + addedCoins },
+        );
+      } else {
+        await Member.create({ memberId: memberId, coins: addedCoins });
+      }
+    }
+
     if (message.content.includes("@everyone" || "@here")) {
       if (!message.member?.permissions.has(PermissionFlagsBits.MentionEveryone))
         message.react("🤡");
