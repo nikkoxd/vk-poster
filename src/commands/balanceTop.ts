8af32b2d74948992ab5bf2308decfc35232549f4
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-02-01 17:02:21 +0400

    fix: use embed color from db

--- src/commands/balanceTop.ts
+++ src/commands/balanceTop.ts
@@ -3,6 +3,7 @@ import { logError } from "..";
 import { t } from "i18next";
 import Member from "../schemas/Member";
 import { EmbedBuilder } from "discord.js";
+import Guild from "../schemas/Guild";
 
 export class balanceTop extends Command {
   public constructor(ctx: Command.LoaderContext, options: Command.Options) {
@@ -20,8 +21,17 @@ export class balanceTop extends Command {
   }
 
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    const guild = await Guild.findOne({ id: process.env.GUILD_ID });
     const members = await Member.find().limit(10).sort({ coins: -1 });
-    const embed = new EmbedBuilder().setColor(`#${process.env.EMBED_COLOR}`);
+    const embed = new EmbedBuilder();
+    if (guild) {
+      embed.setColor(`#${guild.embedColor}`);
+    } else {
+      const guild = new Guild({ id: process.env.GUILD_ID });
+      guild.save();
+
+      embed.setColor(`#${guild.embedColor}`);
+    }
     if (interaction.guild) {
       embed.setTitle(t("commands.balance-top.embed.title"));
       let description = "";
