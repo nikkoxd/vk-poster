5863481f896f89319913b27997ad6a8e08f40204
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-03-09 14:48:23 +0400

    feat: :technologist: add types for env vars

---
+++ src/types/env.d.ts
@@ -0,0 +1,23 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    CLIENT_ID: string;
+    TOKEN: string;
+    DB_USERNAME: string;
+    LOG_CHANNEL: string;
+    OWNER_ID: string;
+    CUSTOM_STATUS: string;
+    BALANCE_ID: string;
+    COMMAND_REMOVE_ID: string;
+    CONFIG_ID: string;
+    GIVE_ID: string;
+    LEADERBOARD_ID: string;
+    PING_ID: string;
+    POLL_ID: string;
+    RANK_ID: string;
+    REWARD_ID: string;
+    SENDMSG_ID: string;
+    SET_ID: string;
+    SHOP_ID: string;
+  }
+}
