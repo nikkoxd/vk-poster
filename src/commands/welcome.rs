3a6b60ed99bcfc2e54327b6006aa8f43e58c21b1
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-01-10 21:47:43 +0400

    fix: change welcome message formatting

--- src/commands/welcome.rs
+++ src/commands/welcome.rs
@@ -3,6 +3,6 @@ use crate::{Context, Error};
 #[poise::command(prefix_command)]
 pub async fn welcome(ctx: Context<'_>) -> Result<(), Error> {
     let user_id = ctx.author().id;
-    ctx.say(format!("Приветик, <@{}>, приветствуем тебя в нашем кафе!", user_id)).await?;
+    ctx.say(format!("Приветик, <@{}>, приветствуем тебя в нашем кафе! <a:hellow:1327303097751572583>", user_id)).await?;
     Ok(())
 }
