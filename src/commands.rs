a107ffcfd14cbda93856b16ebcfd3e9093df2de0
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-01-11 14:49:37 +0400

    fix: move commands to a single file
    later to be catecorized if the file gets too long

---
+++ src/commands.rs
@@ -0,0 +1,59 @@
+use crate::{Context, Error};
+use sqlx::Row;
+
+#[poise::command(slash_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let ping = ctx.ping().await;
+    ctx.say(format!("Pong! Response time: {ping:?}")).await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn setup(ctx: Context<'_>) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().ok_or_else(|| anyhow::anyhow!("Not in a guild"))?;
+    let pool = &ctx.data().pool;
+
+    let guild_exists = sqlx::query("select 1 from guilds where id = $1")
+        .bind(i64::from(guild_id))
+        .fetch_optional(pool)
+        .await?
+        .is_some();
+
+    if !guild_exists {
+        sqlx::query("insert into guilds (id) values ($1)")
+            .bind(i64::from(guild_id))
+            .execute(pool)
+            .await?;
+    }
+
+    Ok(())
+}
+
+#[poise::command(prefix_command)]
+pub async fn welcome(ctx: Context<'_>) -> Result<(), Error> {
+    let user_id = ctx.author().id;
+    let guild_id = ctx.guild_id().ok_or_else(|| anyhow::anyhow!("Not in a guild"))?;
+    let pool = &ctx.data().pool;
+
+    let row = sqlx::query("select welcome_role_id from guilds where id = $1")
+        .bind(i64::from(guild_id))
+        .fetch_one(pool)
+        .await?;
+    let welcome_role_id: Option<i64> = row.try_get("welcome_role_id").ok();
+
+    let mut message = format!("Приветик, <@{}>, приветствуем тебя в нашем кафе! <a:hellow:1327303097751572583>", user_id);
+
+    if let Some(role_id) = welcome_role_id {
+        message.push_str(format!("\n<@&{}>", role_id).as_str());
+    }
+
+    ctx.say(message).await?;
+
+    Ok(())
+}
--- src/commands/ping.rs
+++
@@ -1,8 +0,0 @@
-use crate::{Context, Error};
-
-#[poise::command(slash_command)]
-pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let ping = ctx.ping().await;
-    ctx.say(format!("Pong! Response time: {ping:?}")).await?;
-    Ok(())
-}
