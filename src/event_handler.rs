e991881c7242c62bda77aff1f6e016dcbf52995a
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-01-11 14:49:45 +0400

    fix: add logging to the event handler

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -9,7 +9,7 @@ pub async fn event_handler(
     data: &Data,
 ) -> Result<(), Error> {
     let name = event.snake_case_name();
-    tracing::info!("Event: {name:?}");
+    tracing::info!("Event triggered: {name:?}");
     match event {
         serenity::FullEvent::GuildMemberAddition { new_member } => {
             let user_id = new_member.user.id;
@@ -25,6 +25,8 @@ pub async fn event_handler(
             let welcome_channel_id: Option<i64> = row.try_get("welcome_channel_id").ok();
 
             if let Some(channel_id) = welcome_channel_id {
+                tracing::info!("Welcome channel found: {channel_id:?}");
+
                 let row = sqlx::query("select welcome_role_id from guilds where id = $1")
                     .bind(i64::from(guild_id))
                     .fetch_one(pool)
@@ -34,11 +36,14 @@ pub async fn event_handler(
                 let mut message = format!("Приветик, <@{}>, приветствуем тебя в нашем кафе! <a:hellow:1327303097751572583>", user_id);
 
                 if let Some(role_id) = welcome_role_id {
+                    tracing::info!("Welcome role found: {role_id:?}");
+
                     message.push_str(format!("\n<@&{}>", role_id).as_str());
                 }
 
                 let channels = guild_id.channels(ctx).await?;
                 let channel = channels.get(&ChannelId::new(channel_id.try_into().unwrap()));
+
                 channel.expect("Channel not found").say(ctx, message).await?;
             }
         }
